6db687a90201546804499be8bf1f5a404aa7921d
Daniel Romero <Daniel.Romero@ec.ey.com>
2019-09-29 00:34:44 -0500

    adds a python script

--- python_code.py
+++ python_code.py
@@ -1,9 +1,42 @@
-#My capstone Project
+{
+  "cells":[
+            {
+                "cell_type": "markdown",
+                "metadata":{},
+                "source": [
+                "<h2> My capstone Project"
+                "\n",
+                "This project use GitHub for week1 module 9 of Coursera"
+                ]
+                },
+        {
+        "cell_type": "code",
+        "execution_count": 2,
+                "metadata":{},
+                "outputs": [],
+                "source":[
+                    "import pandas as pd\n",
+                    "import numpy as np",
+                    ]
+                },
+        {
+         "cell_type": "code",
+        "execution_count": 3,
+                "metadata":{},
+                "outputs": [
+                {
+                "data": {
+                    "(Hello Capstone Project Course!)"
+                         },
+                "execution_count": 17,
+                "metadata": {},
+                "output_type": "execute_result"
+                }
+                ],
+                "source":[
+                    "Hello Capstone Project Course!"
+                    ]
+                },
+        ]
+  }
 
-> This project use GitHub week1
-    
-'<import pandas as pd>'
-
-'<import numpy as np>'
-
->print(Hello Capstone Project Course!)
